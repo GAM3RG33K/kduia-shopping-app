4b126215bf8afdb35d729429ae7b14883c4692fd
Harshvardhan Joshi <hj2931996@gmail.co>
2024-07-06 16:48:35 +0530

    Show react components in the Main page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css'; 
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { AppProvider } from './context/AppContext';
 import CartValue from './components/CartValue';
 import ExpenseList from './components/ExpenseList';
@@ -8,8 +8,41 @@ import Location from './components/Location';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <AppProvider>
+      <div className='container'>
+        <h1 className='mt-3'>Shopping App</h1>
+        <div className='row mt-3'>
+          {
+            // CartValue component
+            <div className='col-sm'>
+              <CartValue />
+            </div>
+          }
+
+          {
+            /* Add Location component here*/
+            <div className='col-sm'>
+              <Location />
+            </div>
+          }
+
+          {
+            /* Add ExpenseList component here */
+            <div >
+              <ExpenseList />
+            </div>
+          }
+
+          {
+            /* Add ItemSelected component here under */
+            <div>
+              <ItemSelected />
+            </div>
+          }
+
+        </div>
+      </div>
+    </AppProvider>
   );
 }
 
